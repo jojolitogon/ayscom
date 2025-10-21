f187ea6e8ba9c2436aef07176d0b19cfc38a0162
estelar <estela_romero@ayscom.com>
2025-10-21 13:22:38 +0200

    Bash exercises - exercise 4

---
+++ bash-exercises/bash-exercise-4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+nombres=("Jaime" "Ismael" "Esther" "Adrian" "Jorge" "Juan" "Miguel" "Oscar" "Sergio" "Guilherme" "Pedro" "Pedro C" "Tomas")
+
+for nombre in "${nombres[@]}"; do
+  echo "Nombre: $nombre"
+done
