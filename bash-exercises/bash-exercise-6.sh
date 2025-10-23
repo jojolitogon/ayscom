c7bf36b643e684d38945b45020c356dbd2b8cf0f
estelar <estela_romero@ayscom.com>
2025-10-23 06:34:22 +0200

    changes in bash-exercises.sh

--- bash-exercises/bash-exercise-6.sh
+++ bash-exercises/bash-exercise-6.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cut -d',' -f1,3 csv/sales.csv
+cut -d',' -f1,3 files/sales.csv
 echo ""
-awk -F',' '{print $1, $3}' csv/sales.csv
+awk -F',' '{print $1, $3}' files/sales.csv
 echo ""
-grep "Shoes" csv/sales.csv
+grep "Shoes" files/sales.csv
